ca858e15ae100be385e0328464fb57624a274994
Eahiya <eahiamondal@gmail.com>
2020-08-25 12:35:07 +0600

    heloKhan

---
+++ database/factories/HelloEahiyaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\HelloEahiya;
+use Faker\Generator as Faker;
+
+$factory->define(HelloEahiya::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/seeds/HelloEahiyaSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class HelloEahiyaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
